264d048c291212e1d7d74788c3fa24cc29057cfc
Emily <yday1223@gmail.com>
2022-03-15 23:29:24 +0900

    price-chart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,97 @@
-function Chart() {
-  return <h1>chart</h1>;
+import { useQuery } from 'react-query';
+import { fetchCoinHistory } from '../api';
+import ApexChart from 'react-apexcharts';
+
+interface ChartProps {
+  coinId: string;
+}
+
+interface IHistoricalData {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistoricalData[]>(
+    ['ohlcv', coinId],
+    () => fetchCoinHistory(coinId)
+  );
+  return (
+    <div>
+      {isLoading ? (
+        'Loading chart...'
+      ) : (
+        <ApexChart
+          type='line'
+          series={[
+            {
+              name: 'Price',
+              data: data?.map((price) => price.close) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: 'dark',
+            },
+            chart: {
+              background: 'transparent',
+              height: 500,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+            },
+            grid: {
+              show: false,
+            },
+            stroke: {
+              curve: 'smooth',
+              width: 3,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              type: 'datetime',
+              labels: {
+                show: false,
+                datetimeFormatter: {
+                  month: "MMM 'yy",
+                },
+              },
+              axisBorder: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+
+              categories: data?.map((price) => price.time_close),
+            },
+            fill: {
+              type: 'gradient',
+              gradient: {
+                gradientToColors: ['blue'],
+                stops: [0, 100],
+              },
+            },
+            colors: ['red'],
+            tooltip: {
+              y: {
+                formatter: (value) => `${value.toFixed(2)}`,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
